ba71c2d66a3528489f6d46f29229aecb1cd58c0d
Kim Nguyen <nnhk23@gmail.com>
2020-09-18 01:28:48 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,42 @@
-# require modules here
+require "yaml"
+require 'pry'
+emoticon_file = YAML.load_file('./lib/emoticons.yml')
 
-def load_library
-  # code goes here
+def load_library(emoticon_file)
+  emoticon = YAML.load_file('./lib/emoticons.yml')
+final_result = emoticon.each_with_object({}) do |(key, value), result|
+  result[key] = value
+    result[key] = {
+      :english => value[0],
+      :japanese => value[1]
+    }
+end
+final_result
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(emoticon_file, western_emoticon)
+  emoticon = YAML.load_file('./lib/emoticons.yml')
+  japanese_emoticon = ""
+  load_library(emoticon_file).each do |key, value|
+    if western_emoticon == value[:english]
+      japanese_emoticon = value[:japanese]
+    elsif japanese_emoticon == ""
+      japanese_emoticon = "Sorry, that emoticon was not found"
+    end
+  end
+  japanese_emoticon
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(emoticon_file, japanese_emoticon)
+  english_meaning = ""
+  emoticon = YAML.load_file('./lib/emoticons.yml')
+  load_library(emoticon_file).each do |key, value|
+    if japanese_emoticon == value[:japanese]
+      english_meaning = key
+    elsif english_meaning == ""
+      english_meaning = "Sorry, that emoticon was not found"
+    end
+  end
+  english_meaning
 end
